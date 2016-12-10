8eb0f4c480f871705774ae0497c04f723f91173f
ondro <ondro@ondro-lenovo-flex>
2016-12-10 17:46:52 +0100

    Fixes

--- core/src/main/java/javaeebeauty/core/boundary/Logging.java
+++ core/src/main/java/javaeebeauty/core/boundary/Logging.java
@@ -4,15 +4,15 @@ import java.util.logging.*;
 
 public class Logging {
     public static Logger getLogger(Class<?> cls) {
-        return LogManager.getLogManager().getLogger(cls.getName());
+        return Logger.getLogger(cls.getName());
     }
 
     public static Logger getLogger(Object obj) {
-        return LogManager.getLogManager().getLogger(obj.getClass().getName());
+        return Logger.getLogger(obj.getClass().getName());
     }
 
     public static Logger getRootLogger() {
-        return LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
+        return Logger.getGlobal();
     }
     
 }
